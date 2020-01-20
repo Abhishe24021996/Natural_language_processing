5af4819524baf9ed77125ae1b80db0243fdbe3a7
Abhishek Chauhan <47885830+Abhishe24021996@users.noreply.github.com>
2020-01-20 17:58:34 +0530

    Update 7_extraction_information_from_text.py

--- 7_extraction_information_from_text.py
+++ 7_extraction_information_from_text.py
@@ -97,3 +97,24 @@ def pos_features(sentence,i):
   else:
     features["prev-word"] = sentence[i-1]
 return features
+
+             
+#sentence segmentor
+sents = nltk.corpus.treebank_raw.sents()
+tokens =[]
+boundaries = set()
+offset = 0
+for sent in sents:
+  tokens.extend(sent)
+  offset += len(sent)
+  boundaries.add(offset-1)
+             
+def punct_features(tokens, i ):
+  return {'next-word-capitalised' : tokens[i+1][0].isupper(),
+          'prevword':tokens[i-1].lower()
+          'punct':tokens[i],
+          'prev-word-is-one-char': len(tokens[i-1])==1}
+             
+fs = [(punct_features(tokens,i), (i in boundaries))
+      for i in range(1, len(tokens)-1)
+      if tokens[i] in '.?!']
